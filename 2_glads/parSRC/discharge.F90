0d8aa4cdcda9154b8d4c742ace7976b464a09ae9
nikolajovanovic98 <nikola.jovanovic@fau.de>
2025-07-04 10:59:20 +0200

    Updated discharge and minelev mask

--- 2_glads/parSRC/discharge.F90
+++ 2_glads/parSRC/discharge.F90
@@ -18,19 +18,15 @@ SUBROUTINE get_Discharge( Model, Solver, dt, Transient )
     TYPE(ValueList_t), POINTER  :: SolverParams
 
     ! Initialise variables 
-    TYPE(Element_t), POINTER    :: Edge ! Element
-    TYPE(Variable_t), POINTER   :: SheetVar, SheetDisVar, ChannelVar, ZbVar, HydPotVar
-    INTEGER, POINTER            :: SheetPerm(:), SheetDisPerm(:), ChannelPerm(:), ZbPerm(:), HydPotPerm(:)
-    REAL(KIND=dp), POINTER      :: SheetVals(:), SheetDisVals(:), ChannelVals(:), ZbVals(:), HydPotVals(:)
+    TYPE(Element_t), POINTER    :: Edge
+    TYPE(Variable_t), POINTER   :: SheetVar, SheetDisVar, ChannelVar, MaskVar
+    INTEGER, POINTER            :: SheetPerm(:), SheetDisPerm(:), ChannelPerm(:), MaskPerm(:)
+    REAL(KIND=dp), POINTER      :: SheetVals(:), SheetDisVals(:), ChannelVals(:), MaskVals(:)
 
-    INTEGER                     :: nn, ee, numNodes, terminusNode, node, ierr, localterminus
+    INTEGER                     :: nn, ee, numNodes, terminusNode
     CHARACTER(LEN=MAX_NAME_LEN) :: SolverName
-    CHARACTER(LEN=20)           :: ierr_string
-
-    LOGICAL                     :: GotIt
 
     ! Values to calculate
-    REAL(KIND=dp)               :: lowestElev, bedElevationValue, globalElev
     REAL(KIND=dp)               :: volFluxSheet, volFluxChannel, SheetDisMag
 
     ! Variables to calculate
@@ -58,22 +54,6 @@ SUBROUTINE get_Discharge( Model, Solver, dt, Transient )
     TerminusFluxVals => Solver % Variable % Values
     TerminusFluxPerm => Solver % Variable % Perm
 
-    !terminusSheetVar => VariableGet(Model % Mesh % Variables, 'Sheet Outflow')
-    !IF ( ASSOCIATED(terminusSheetVar)) THEN 
-    !    terminusSheetPerm => terminusSheetVar % Perm
-    !    terminusSheetVals => terminusSheetVar % Values
-    !ELSE
-    !    CALL FATAL(SolverName, 'Variable Sheet Outflow not found')
-    !END IF 
-
-    !terminusChannelVar => VariableGet(Model % Mesh % Variables, 'Channel Outflow')
-    !IF ( ASSOCIATED(terminusChannelVar )) THEN 
-    !    terminusChannelPerm => terminusChannelVar % Perm 
-    !    terminusChannelVals => terminusChannelVar % Values
-    !ELSE 
-    !    CALL FATAL(SolverName, 'Variable Channel Outflow not found')
-    !END IF 
-    
     ! Variables containing the GlaDS sheet thickness and discharge and channel flux 
 
     SheetVar => VariableGet(Model % Mesh % Variables, 'Sheet Thickness')
@@ -86,7 +66,7 @@ SUBROUTINE get_Discharge( Model, Solver, dt, Transient )
 
     SheetDisVar => VariableGet(Model % Mesh % Variables, 'Sheet Discharge')
     IF ( ASSOCIATED( SheetDisVar) ) THEN
-        SheetPerm    => SheetDisVar % Perm
+        SheetDisPerm => SheetDisVar % Perm
         SheetDisVals => SheetDisVar % Values
     ELSE
         CALL FATAL(SolverName, 'Variable Sheet Discharge not found')
@@ -100,89 +80,44 @@ SUBROUTINE get_Discharge( Model, Solver, dt, Transient )
         CALL FATAL(SolverName, 'Variable Channel Flux not found')
     END IF
 
-    ZbVar => VariableGet(Model % Mesh % Variables, TRIM('ubed'))
-    IF ( ASSOCIATED( ZbVar) ) THEN
-        ZbPerm => ZbVar % Perm
-        ZbVals => ZbVar % Values
-        !PRINT *, ZbVals
+    MaskVar => VariableGet(Model % Mesh % Variables, 'LowestElevationMask')
+    IF ( ASSOCIATED (MaskVar)) THEN 
+        MaskPerm => MaskVar % Perm 
+        MaskVals => MaskVar % Values
     ELSE
-        CALL FATAL(SolverName, 'Variable Zb not found')
-    END IF
-
-    HydPotVar => VariableGet(Model % Mesh % Variables, 'Hydraulic Potential')
-    IF ( ASSOCIATED (HydPotVar)) THEN 
-        HydPotPerm => HydPotVar % Perm 
-        HydPotVals => HydPotVar % Values
-    ELSE
-        CALL FATAL(SolverName, 'Variable Hydraulic Potential not found')
+        CALL FATAL(SolverName, 'Variable LowestElevationMask not found')
     END IF
 
     ! Loop over all nodes
     numNodes = Solver % Mesh % Nodes % NumberOfNodes 
 
-    terminusNode   = -1
-    lowestElev     = 10000. 
-    volFluxSheet   = 0
-    volFluxChannel = 0
+    terminusNode    = -1
 
-
-    DO ee=1, Solver % Mesh % NumberOfEdges 
-        Edge => Solver % Mesh % Edges(ee)
-
-        ! Loop through all nodes associated with the current edge
-        DO nn=1, SIZE(Edge % NodeIndexes)
-
-            node = Edge % NodeIndexes(nn)
-
-            ! Access the permuted bed elevation value
-            bedElevationValue = ZbVals(ZbPerm(node))
-            !PRINT *, bedElevationValue
-
-            ! Check if this node has the lowest bed elevation
-            IF (bedElevationValue < lowestElev) THEN
-                lowestElev = bedElevationValue      ! Update lowest elevation
-                terminusNode = node                 ! Mark this node as the terminus node
-            END IF
-        END DO
-    END DO
-
-    PRINT *, "sheetdisperm: ", SheetDisPerm(terminusNode)
-
-    ! Parallel run 
-    IF (ParEnv % PEs > 1) THEN 
-        CALL MPI_ALLREDUCE(lowestElev, globalElev, 1, MPI_DOUBLE_PRECISION, MPI_MIN, ELMER_COMM_WORLD, ierr)
+    DO nn=1, numNodes
         
-        IF (ierr /= 0) THEN 
-                
-            WRITE(ierr_string, '(I5)') ierr
-            CALL FATAL(SolverName, "MPI_ALLREDUCE failed with error code " // TRIM(ierr_string))
+        IF (MaskVals(MaskPerm(nn)) == 1) THEN 
 
-        END IF 
+            terminusNode = nn
 
-        PRINT *, "Global elevation: ", globalElev
-        
-        IF (globalElev == lowestElev) THEN 
-
-            PRINT *, "Lowest elev :", lowestElev
-            PRINT *, "ParEnv % PEs:", ParEnv % PEs
-            PRINT *, "terminusNode:", terminusNode
-            PRINT *, "SheetDisPerm(terminusNode):", SheetDisPerm(terminusNode)
-            !PRINT *, "Accessing SheetDisVals at index:", 2*(SheetDisPerm(terminusNode)-1) + 1
-            !PRINT *, "Accessing ChannelVals at index:", numNodes + ee
-
+            ! initalise
+            volFluxSheet    = 0
+            volFluxChannel  = 0
+            
+            ! Sheet discharge magnitude
             SheetDisMag = ( SheetDisVals(2*(SheetDisPerm(terminusNode)-1) + 1)**2.0 +   &
                             SheetDisVals(2*(SheetDisPerm(terminusNode)-1) + 2)**2.0 )**0.5
 
             ! Volume flux from the sheet is given by multiplying sheet discharge and sheet thk
             volFluxSheet = SheetVals(SheetPerm(terminusNode)) * SheetDisMag
 
+            ! Loop over all edges
             DO ee=1, Solver % Mesh % NumberOfEdges
 
                 Edge => Solver % Mesh % Edges(ee)
 
                 IF  (Edge % NodeIndexes(1).EQ.terminusNode .OR. Edge % NodeIndexes(2).EQ.terminusNode) THEN 
 
-                    ! Halve values for edges at partition boundaries bevause these
+                    ! Halve values for edges at partition boundaries because these
                     ! will be counted twice 
                     IF (Solver % Mesh % ParallelInfo % EdgeInterface(ee)) THEN
                         volFluxChannel = volFluxChannel + 0.5*ChannelVals(ChannelPerm(numNodes + ee))
@@ -195,59 +130,10 @@ SUBROUTINE get_Discharge( Model, Solver, dt, Transient )
             END DO
 
             TerminusFluxVals(TerminusFluxPerm(terminusNode))       = volFluxChannel + volFluxSheet
-            PRINT *, "Elevation is: ", lowestElev
-            !PRINT *, "Volume flux from the sheet is: ", volFluxSheet
+            PRINT *, "Volume flux from the sheet is: ", volFluxSheet
             PRINT *, "Volume flux from the channels is: ", volFluxChannel
             PRINT *, "Volume flux across terminus is: ", TerminusFluxVals(TerminusFluxPerm(terminusNode))
-        END IF 
-    END IF 
-    
-    IF (ParEnv % PEs == 1) THEN 
-        IF (terminusNode /= -1) THEN 
-
-            !PRINT *, "terminusNode:", terminusNode
-
-            !PRINT *, "SheeDis: ", SheetDisPerm(terminusNode)
-            ! This is the calculation if the Sheet Discharge is always a 2D vector. Does it work here?
-            ! Sometimes it gives a segmentation fault.
-            SheetDisMag = ( SheetDisVals(2*(SheetDisPerm(terminusNode)-1) + 1)**2.0 +   &
-                            SheetDisVals(2*(SheetDisPerm(terminusNode)-1) + 2)**2.0 )**0.5
-
-            ! Volume flux from the sheet is given by multiplying sheet discharge and sheet thk
-            volFluxSheet = SheetVals(SheetPerm(terminusNode)) * SheetDisMag
-
-            DO ee=1, Solver % Mesh % NumberOfEdges
-
-                Edge => Solver % Mesh % Edges(ee)
-
-                IF  (Edge % NodeIndexes(1).EQ.terminusNode .OR. Edge % NodeIndexes(2).EQ.terminusNode) THEN 
-            
-                    !IF (Solver % Mesh % ParallelInfo % EdgeInterface(ee)) THEN
-                    !IF (ParEnv % PEs > 1) THEN 
-                        ! Halve values for edges at partition boundaries bevause these
-                        ! will be counted twice 
-                    !    volFluxChannel = volFluxChannel + 0.5*ChannelVals(ChannelPerm(numNodes + ee))
-                    !ELSE 
-                        volFluxChannel = volFluxChannel + ChannelVals(ChannelPerm(numNodes + ee))
-                    
-                    !END IF
-
-                END IF
-
-            END DO
-
-        END IF
-    END IF
-    
-    !terminusSheetVals(terminusSheetPerm(terminusNode))     = volFluxSheet
-    !terminusChannelVals(terminusChannelPerm(terminusNode)) = volFluxChannel
-    
-    !                                                          terminusSheetVals(terminusSheetPerm(terminusNode)) + &
-    !                                                         terminusChannelVals(terminusChannelPerm(terminusNode))
-    !PRINT *, "Terminus node is: ", localterminus
-    !PRINT *, "Elevation is: ", lowestElev
-    !PRINT *, "Volume flux from the sheet is: ", volFluxSheet
-    !PRINT *, "Volume flux from the channels is: ", volFluxChannel
-    !PRINT *, "Volume flux across terminus is: ", TerminusFluxVals(TerminusFluxPerm(terminusNode))
+        END IF  
+    END DO
 
 END SUBROUTINE get_Discharge
--- 2_glads/parSRC/minelevmask.F90
+++ 2_glads/parSRC/minelevmask.F90
@@ -1,5 +1,5 @@
 !------------------------------------------------------------------------------
-! File: discharge.f90
+! File: minelevmask.f90
 ! Written by: Nikola Jovanovic, 5 March 2025
 ! Modified by: -
 ! To compile: elmerf90 -o minelevmask.so minelevmask.F90
@@ -15,12 +15,15 @@ SUBROUTINE minelevmask( Model, Solver, dt, Transient )
     LOGICAL        :: Transient
 
     ! Load variables
-    TYPE(Variable_t), POINTER   :: ubedVar
-    INTEGER, POINTER            :: ubedPerm(:)
-    REAL(KIND=dp), POINTER      :: ubedVals(:)
+    TYPE(Variable_t), POINTER   :: ubedVar, usurfVar
+    INTEGER, POINTER            :: ubedPerm(:), usurfPerm(:)
+    REAL(KIND=dp), POINTER      :: ubedVals(:), usurfVals(:)
 
-    INTEGER                     :: nn, totalNodes, terminusNode=-1, node
-    REAL(KIND=dp)               :: bedElevationValue, min_ubed = 10000.
+    TYPE(Mesh_t), POINTER       :: Mesh
+    TYPE(Element_t), POINTER    :: Element
+
+    INTEGER                     :: nn, totalNodes, node, t, j, n, bc_id, nodeid
+    REAL(KIND=dp)               :: bedElevationValue, min_ubed = 10000., surfElevationValue, min_usurf = 10000.
     CHARACTER(LEN=MAX_NAME_LEN) :: SolverName
     CHARACTER(LEN=20)           :: ierr_string
 
@@ -30,7 +33,7 @@ SUBROUTINE minelevmask( Model, Solver, dt, Transient )
     REAL(KIND=dp), POINTER      :: LowestElevMaskVals(:) 
 
     ! MPI Variables
-    REAL(KIND=dp)               :: global_min_usurf
+    REAL(KIND=dp)               :: global_min_usurf, global_min_ubed
     INTEGER                     :: ierr
 
     !------------------------------------------------------------
@@ -52,39 +55,76 @@ SUBROUTINE minelevmask( Model, Solver, dt, Transient )
         CALL FATAL(SolverName, "Variable ubed not found")
     END IF 
 
+    usurfVar => VariableGet(Model % Mesh % Variables, TRIM("usurf"))
+    IF( ASSOCIATED( usurfVar ) ) THEN 
+        usurfPerm => usurfVar % Perm
+        usurfVals => usurfVar % Values
+    ELSE
+        CALL FATAL(SolverName, "Variable usurf not found")
+    END IF 
+    
+    
     totalNodes = Model % Mesh % NumberOfNodes
 
-    PRINT *, "total nodes = ", totalNodes
-    DO nn=1, totalNodes 
+    ! loop over boundary elements
+    DO t=1, Model % Mesh % NumberOfBoundaryElements
 
-            node = nn
+        ! get element information
+        Element => GetBoundaryElement(t)
 
-            ! Access bed elev
-            bedElevationValue = ubedVals(ubedPerm(node))
+        bc_id = GetBCId(Element)
 
-            ! Check if this node has the lowest bed elev
-            IF (bedElevationValue < min_ubed) THEN
-                min_ubed = bedElevationValue    ! Update lowest elevation
-                !terminusNode = node             ! Mark this node as the terminus node
-            END IF
+        ! access edge BC
+        IF (bc_id /= 1) CYCLE
+
+        n = GetElementNOFNodes()
+
+        ! loop over element nodes
+        DO j=1, n
+            
+            ! get node indices
+            nodeid = Element % NodeIndexes(j)
+
+            ! get surf and bed elev values
+            bedElevationValue = ubedVals(ubedPerm(nodeid))
+            surfElevationValue = usurfVals(usurfPerm(nodeid))
+
+            ! min ubed logic
+            IF (bedElevationValue < min_ubed) THEN 
+
+                min_ubed = bedElevationValue
+
+            END IF 
+
+            ! min usurf logic
+            IF (surfElevationValue < min_usurf) THEN 
+
+                min_usurf = surfElevationValue
+            
+            END IF 
+        
+        END DO
     END DO
 
-    PRINT *, "Min ubed = ", min_ubed
-
+    ! get global lowest elev for partitioned mesh
     IF (ParEnv % PEs > 1) THEN 
-        CALL MPI_ALLREDUCE(min_ubed, global_min_usurf, 1, MPI_DOUBLE_PRECISION, MPI_MIN, ELMER_COMM_WORLD, ierr)
+        CALL MPI_ALLREDUCE(min_ubed, global_min_ubed, 1, MPI_DOUBLE_PRECISION, MPI_MIN, ELMER_COMM_WORLD, ierr)
+        CALL MPI_ALLREDUCE(min_usurf, global_min_usurf, 1, MPI_DOUBLE_PRECISION, MPI_MIN, ELMER_COMM_WORLD, ierr)
+
         IF (ierr /= 0) THEN 
             WRITE(ierr_string, '(I5)') ierr
             CALL FATAL(SolverName, "MPI_ALLREDUCE failed with error code " // TRIM(ierr_string))
         END IF
-        min_ubed = global_min_usurf
+        min_ubed = global_min_ubed
+        min_usurf = global_min_usurf
     END IF
 
-
+    ! get lowest elevation mask
     DO nn=1, totalNodes 
         node = nn
         bedElevationValue = ubedVals(ubedPerm(node))
-        IF (bedElevationValue == min_ubed) THEN
+        surfElevationValue = usurfVals(usurfPerm(node))
+        IF (surfElevationValue == min_usurf) THEN 
 
             LowestElevMaskVals(LowestElevMaskPerm(node)) = 1
 
@@ -96,4 +136,4 @@ SUBROUTINE minelevmask( Model, Solver, dt, Transient )
 
     END DO 
 
-    END SUBROUTINE minelevmask
+    END SUBROUTINE minelevmask  
